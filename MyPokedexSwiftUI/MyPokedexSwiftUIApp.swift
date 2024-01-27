ea70a7b190761d5550d791a3188f79431e61e399
Bruno Marques <bruno.marques@townsq.com.br>
2024-01-27 18:30:21 -0300

    first commit

---
+++ MyPokedexSwiftUI/MyPokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPokedexSwiftUIApp.swift
+//  MyPokedexSwiftUI
+//
+//  Created by Bruno Marques on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MyPokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
